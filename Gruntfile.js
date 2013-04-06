107f852ca3e5e18baf948e6cd3599397d3071dc1
David Smith <codeactual@gmail.com>
2013-04-06 11:57:16 -0700

    chore(test): Launch tests from mocha
    Allow assertion of code/output, runtime duration (for timeouts), etc.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,13 +5,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-shell');
 
-  var baseConjureCmd =
-    'bin/conjure ' +
-      '--server bin/test-server ' +
-      '--root-dir ' + __dirname + ' ' +
-      '--verbose ';
-  var baseConjureOpt = {stdout: true, stderr: true, failOnError: false};
-
   grunt.initConfig({
     jshint: {
       src: {
@@ -56,9 +49,7 @@ module.exports = function(grunt) {
       }
     },
     shell: {
-      options: {
-        failOnError: true
-      },
+      options: { failOnError: true },
       build: {
         command: 'component install --dev && component build --standalone conjure --name conjure --out dist --dev'
       },
@@ -68,31 +59,9 @@ module.exports = function(grunt) {
       shrinkwrap: {
         command: 'npm shrinkwrap'
       },
-      test_bootstrap: {
-        options: baseConjureOpt,
-        command:
-          baseConjureCmd +
-          '--bootstrap test/fixture/custom-bootstrap.js ' +
-          '--grep-file "^bootstrap\\.js$" '
-      },
-      test_empty_test: {
-        options: baseConjureOpt,
-        command:
-          baseConjureCmd +
-          '--grep-file "^empty-test\\.js$" '
-      },
-      test_flow: {
-        options: baseConjureOpt,
-        command:
-          baseConjureCmd +
-          '--grep-file "^flow\\.js$" '
-      },
-      test_grepv_case: {
-        options: baseConjureOpt,
-        command:
-          baseConjureCmd +
-          '--grep-file "^grepv-case\\.js$" ' +
-          '--grepv-case should prevent this from running'
+      test_bin: {
+        options: { stdout: true, stderr: true, failOnError: true },
+        command: 'mocha --reporter tap test/index.js'
       }
     }
   });
@@ -100,11 +69,5 @@ module.exports = function(grunt) {
   grunt.registerTask('default', ['jshint', 'shell:shrinkwrap']);
   grunt.registerTask('build', ['shell:build']);
   grunt.registerTask('dist', ['shell:dist', 'uglify:dist']);
-  grunt.registerTask('test_bin', [
-    'build',
-    'shell:test_bootstrap',
-    'shell:test_empty_test',
-    'shell:test_flow',
-    'shell:test_grepv_case'
-  ]);
+  grunt.registerTask('test_bin', ['build', 'shell:test_bin']);
 };
