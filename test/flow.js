c8385bbd37b46eb38764fb11cb45029d76260b6b
David Smith <codeactual@gmail.com>
2013-04-06 10:08:20 -0700

    Remove unused describe()

--- test/flow.js
+++ test/flow.js
@@ -1,12 +1,12 @@
 module.exports = function(conjure) {
   'use strict';
 
+  // TODO focus this script on properties available at different flow contexts
+
   conjure.test('flow', function() {
     this.describe('Conjure', function() {
-      this.describe('url()', function() {
-        this.it('should pass' , function() {
-          this.test.assertEquals(1, 1);
-        });
+      this.it('should pass' , function() {
+        this.test.assertEquals(1, 1);
       });
     });
   });
