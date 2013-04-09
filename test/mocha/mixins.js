4e2259331338e0ad68e0270fa3e2fb25d1b70a5a
David Smith <codeactual@gmail.com>
2013-04-08 17:32:20 -0700

    test(mixins): Refactor/DRY-fixes

--- test/mocha/mixins.js
+++ test/mocha/mixins.js
@@ -16,24 +16,33 @@ describe('mixins', function() {
 
   beforeEach(function() {
     this.conjure = conjure.create();
-    this.conjure.casper = {};
     this.conjureStub = this.stub(this.conjure);
-    GLOBAL.$ = function() {};
+
+    this.conjure.casper = {};
+
+    // Use for stubs like: $(sel).<some method>()
     this.$resStub = {};
+
+    // Use this.$stub to know what selector $ received.
+    GLOBAL.$ = function() {};
     this.$stub = this.stub(GLOBAL, '$').returns(this.$resStub);
+
+    // Collection of fixtures and more prepared stubs.
+    this.sel = '.klass';
+    this.evalStub = this.stubMany(this.conjure.casper, 'thenEvaluate').thenEvaluate;
+    this.clickStub = this.stubMany(this.$resStub, 'click').click;
   });
 
   describe('click', function() {
     beforeEach(function() {
-      this.sel = '.klass';
-      this.evalStub = this.stubMany(this.conjure.casper, 'thenEvaluate').thenEvaluate;
       this.evalStub.yields(this.sel);
       this.conjureStub.click.restore();
-      this.clickStub = this.stubMany(this.$resStub, 'click').click;
       this.conjure.click(this.sel);
     });
     it('should wait for selector match' , function() {
       this.conjureStub.selectorExists.calledWithExactly(this.sel);
+    });
+    it('should use jQuery to click' , function() {
       this.evalStub.should.be.called;
       this.$stub.should.have.been.calledWithExactly(this.sel);
       this.clickStub.should.have.been.called;
