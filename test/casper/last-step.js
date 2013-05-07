0e3af43d15d4981834738427c6ec102f67e73488
David Smith <codeactual@gmail.com>
2013-05-06 22:57:55 -0700

    test(trace): Add last-step casper test
    - Its output will be asserted from test/mocha/bin.js

--- test/casper/last-step.js
+++ test/casper/last-step.js
@@ -32,6 +32,47 @@ module.exports = function(conjure) {
       });
     });
 
+    this.describe('each', function() {
+      this.it('should tag last step' , function() {
+        this.conjure.each([1], function() {
+          this.test.assertEquals(1, 1);
+        });
+      });
+    });
+
+    this.describe('openHash', function() {
+      this.it('should tag last step when not waiting for selector' , function() {
+        this.conjure.openHash('#foo');
+      });
+      this.it('should tag last step when waiting for selector' , function() {
+        this.conjure.openHash('#foo', 'body');
+      });
+    });
+
+    this.describe('openInitUrl', function() {
+      this.it('should tag last step' , function() {
+        this.conjure.openInitUrl();
+      });
+    });
+
+    this.describe('selectorExists', function() {
+      this.it('should tag last step' , function() {
+        this.conjure.selectorExists('body');
+      });
+    });
+
+    this.describe('selectorMissing', function() {
+      this.it('should tag last step' , function() {
+        this.conjure.selectorMissing('.not-present');
+      });
+    });
+
+    this.describe('sendKeys', function() {
+      this.it('should tag last step' , function() {
+        this.conjure.sendKeys('body', 'foo');
+      });
+    });
+
     this.describe('next test', function() {
       this.it('should trigger error' , function() { var a; a.b = 'foo'; });
     });
